d669e4b9a3c1b3196ec4eebf079ad64a9cdb9f7c
Div_YT <comedhe83@gmail.com>
2023-07-16 07:33:50 +0200

    Some Changes(logo etc)

--- frontend/src/components/shared/RegisterForm/RegisterForm.js
+++ frontend/src/components/shared/RegisterForm/RegisterForm.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.png';
 import './RegisterForm.css';
-import { Link } from 'react-router-dom';
+import {Link, redirect} from 'react-router-dom';
 
 const RegisterForm = () => {
     return (
         <div className="RegisterForm">
-            <Link to="/">
+            <Link to='https://fr.legacy.reactjs.org/'>
                 <img src={logo} alt="logo" className="Register-logo" />
             </Link>
             <div className="h1 text-center">School App</div>
